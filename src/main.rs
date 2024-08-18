0ff5c55fc5d9ca9e0c536fcb6451737b9708e51b
gignsky <gignsky@gmail.com>
2024-08-18 14:53:20 -0400

    Refactor square importer and add support for UltraRow

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,34 @@
 #[allow(unused_imports)]
 use tinyfiledialogs as tfd;
 
-mod importers;
+mod import;
 mod utils;
 
 #[allow(unused_mut)]
 fn main() {
-    // println!("{:#?}", &paths);
-    // paths.output = Option::Some(String::from(
-    //     "/home/gig/local_repos/white_horse_tables/output_files/",
-    // ));
-    // println!("{:#?}", &paths);
     let mut paths = get_paths();
-    // println!("{:#?}", &paths);
+    // println!("Inital paths var: {:#?}", &paths);
+    // paths.output = Some("/home/gig/local_repos/white_horse_tables/output_files/".to_string());
     // let tables =
-    importers::import_files(paths);
+    import::import_files(paths);
 }
 
 fn get_paths() -> utils::FilePaths {
     let square_file_path =
-        Some("/home/gig/local_repos/white_horse_tables/source_files/".to_string());
+        Some("/home/gig/local_repos/white_horse_tables/resources/small-copies/Square - items-2024-07-15-2024-08-09.csv".to_string());
 
-    //     let tickera_file_path = path_packer(
-    //         "/home/gig/local_repos/white_horse_tables/source_files/".to_string(),
-    //     );
-    //
-    //     let woo_commerce_file_path = path_packer(
-    //         "/home/gig/local_repos/white_horse_tables/source_files/".to_string(),
-    //     );
+    let tickera_file_path = Some(
+            "/home/gig/local_repos/white_horse_tables/resources/small-copies/Tickera - Attendee List (24).csv".to_string(),
+        );
+
+    let woo_commerce_file_path = Some(
+            "/home/gig/local_repos/white_horse_tables/resources/small-copies/WooCommerce - wc-orders-report-export-17232178944415.csv".to_string(),
+        );
 
     utils::FilePaths {
-        square: square_file_path,
-        tickera: None,      // tickera_file_path,
-        woo_commerce: None, // woo_commerce_file_path,
+        square: square_file_path,             // square_file_path,
+        tickera: tickera_file_path,           // tickera_file_path,
+        woo_commerce: woo_commerce_file_path, // woo_commerce_file_path,
         output: None,
     }
 }
