85c0023c26243943d0e6e355a27e49c79800fdf0
gignsky <gignsky@gmail.com>
2024-08-18 18:21:57 -0400

    updated name to use hyphens

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,23 @@
-#[allow(unused_imports)]
-use tinyfiledialogs as tfd;
+// // `error_chain!` can recurse deeply
+// #![recursion_limit = "1024"]
+//
+// // Import the macro. Don't forget to add `error-chain` in your
+// // `Cargo.toml`!
+// #[macro_use]
+// extern crate error_chain;
+//
+// // We'll put our errors in an `errors` module, and other modules in
+// // this crate will `use errors::*;` to get access to everything
+// // `error_chain!` creates.
+// mod errors {
+//     error_chain! {} // Create the Error, ErrorKind, ResultExt, and Result types
+// }
+//
+// // This only gives access within this module. Make this `pub use errors::*;`
+// // instead if the types must be accessible from other modules (e.g., within
+// // a `links` section).
+// #[allow(unused_imports)]
+// use errors::*;
 
 mod import;
 mod utils;
