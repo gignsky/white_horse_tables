75342a1a81a64b7a9211ae7c474c82e5ade22e7e
gignsky <gignsky@gmail.com>
2024-08-18 14:05:34 -0400

    chore: Refactor square importer and add support for UltraRow

--- src/importers/square.rs
+++ src/importers/square.rs
@@ -1,11 +1,15 @@
 use dollars::Dollars;
+use serde::{Deserialize, Deserializer};
 use std::path::PathBuf;
+use std::{error::Error, io, process};
 
+#[derive(Debug, serde::Deserialize)]
 enum Category {
     Bar,
     None,
 }
 
+#[derive(Debug, serde::Deserialize)]
 enum Item {
     EmployeeBeverage,
     EmployeeSnacks,
@@ -17,28 +21,34 @@ enum Item {
     None,
 }
 
+#[derive(Debug, serde::Deserialize)]
 enum EventType {
     Payment,
 }
 
+#[derive(Debug, serde::Deserialize)]
 enum Location {
     WhiteHorseBar,
 }
 
+#[derive(Debug, serde::Deserialize)]
 struct Customer {
     customer_id: String,
     customer_name: String,
     customer_reference_id: String,
 }
 
+#[derive(Debug, serde::Deserialize)]
 enum Unit {
     Ea,
 }
 
+#[derive(Debug, serde::Deserialize)]
 enum ItemType {
     PhsyicalItem,
 }
 
+#[derive(Debug, serde::Deserialize)]
 struct SquareRow {
     name: String,
     time: String,
@@ -71,6 +81,20 @@ struct SquareRow {
     token: String,
 }
 
+impl Deserialize<'_> for Dollars {
+    fn deserialize<'de, D>(deserializer: D) -> Result<Dollars, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let s = String::deserialize(deserializer)?;
+        let mut parts = s.split('.');
+        let dollars = parts.next().unwrap_or("0").parse::<i64>().unwrap_or(0);
+        let cents = parts.next().unwrap_or("0").parse::<i64>().unwrap_or(0);
+        Ok(Dollars::from(dollars * 100 + cents))
+    }
+}
+
+#[derive(Debug, serde::Deserialize)]
 struct UltraRow {
     id: u64,
     row: SquareRow,
@@ -90,5 +114,17 @@ pub fn import(file_path: Option<PathBuf>) {
     };
     println!("{:?}", path);
 
-    
+    let csv = read_in(path);
+
+    println!("{:?}", csv);
+}
+
+fn read_in(path: PathBuf) -> Result<(), Box<dyn Error>> {
+    let file = std::fs::File::open(path)?;
+    let mut rdr = csv::Reader::from_reader(file);
+    for result in rdr.records() {
+        let record: SquareRow = result?;
+        println!("{:?}", record);
+    }
+    Ok(())
 }
